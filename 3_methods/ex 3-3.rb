b74d145aae144eee7b83410a4345593c2a1bea5d
Michael Fessler <michael.fessler@gmail.com>
2018-09-14 15:44:27 -0400

    chapter 3 done

---
+++ 3_methods/ex 3-3.rb
@@ -0,0 +1,7 @@
+#Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+
+def mult(a,b)
+  a*b
+end
+
+puts mult(67,3)
---
+++ 3_methods/ex 3-5.rb
@@ -0,0 +1,11 @@
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+rc=scream("Yippeee")
+puts "Return code=#{rc}" # It's nil because puts returns nil
+
+
+
